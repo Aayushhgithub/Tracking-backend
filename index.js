7d556fa7cbb22b9350a90ba4965dd145e1beefd7
Aayushhgithub <ayushtiwari1802@gmail.com>
2025-05-13 23:47:35 +0530

    realtime-backend

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+    socket.on("client-location-send", (data)=>{
+      console.log("data", data);
+      io.emit("server-location-send", data);
+    })
+});
+
+server.listen(8000, () => {
+    console.log("Server running on port 8000");
+});
